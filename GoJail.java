445368aac8dd157253fb299374e04304fd3973e9
codeisma <78846716+codeisma@users.noreply.github.com>
2021-03-02 13:51:21 +0100

    Update GoJail.java

--- GoJail.java
+++ GoJail.java
@@ -12,7 +12,7 @@ public class GoJail {
 
         int yearP1 =0;
         int yearP2 =0;
-        int turn =0;
+    
 
             System.out.println("P1: B for Betray S for Silent!!!!");
             p1 = silentBetray.next();
